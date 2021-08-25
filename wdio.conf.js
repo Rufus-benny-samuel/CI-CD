6b4cd3c3f5aef5b5b70988cb0bb4e811383476b5
rufusbennysamuel <rufus.robert@anywhere.co>
2021-08-25 19:19:56 +0530

    test

--- wdio.conf.js
+++ wdio.conf.js
@@ -54,15 +54,16 @@ exports.config = {
         // maxInstances can get overwritten per capability. So if you have an in-house Selenium
         // grid with only 5 firefox instances available you can make sure that not more than
         // 5 instances get started at a time.
-        maxInstances: 5,
-        //
-        browserName: 'chrome',
-        acceptInsecureCerts: true
-        // If outputDir is provided WebdriverIO can capture driver session logs
-        // it is possible to configure which logTypes to include/exclude.
-        // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
-        // excludeDriverLogs: ['bugreport', 'server'],
-    }],
+        'maxInstances': 1,
+        'browserName': 'chrome',
+        'goog:chromeOptions': {
+           args: ['--headless', '--disable-gpu'],
+          prefs: {
+            'profile.managed_default_content_settings.popups': 1,
+            'profile.managed_default_content_settings.notifications': 1,
+          },
+        },
+      },],
     //
     // ===================
     // Test Configurations
